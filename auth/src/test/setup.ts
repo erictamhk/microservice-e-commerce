32c1253da0ffefe9604d5c017897c8622aa97d0a
Eric Tam <eric.m2hk@gmail.com>
2021-10-17 04:13:43 -0700

    first test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,7 +4,8 @@ import { app } from "../app";
 
 let mongo: MongoMemoryServer;
 beforeAll(async () => {
-  mongo = new MongoMemoryServer();
+  process.env.JWT_KEY = "testing-key";
+  mongo = await MongoMemoryServer.create();
   const mongoUri = await mongo.getUri();
 
   await mongoose.connect(mongoUri);
