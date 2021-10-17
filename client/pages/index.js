d77464c04f8ff41575ce4e37a1b813821e55fa72
Eric Tam <eric.m2hk@gmail.com>
2021-10-17 16:31:26 -0700

    get currenct user in the landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,33 @@
-const index = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+  let url;
+  let option;
+  if (typeof window === "undefined") {
+    console.log("work on server");
+    url = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
+    option = {
+      headers: req.headers,
+    };
+  } else {
+    console.log("work on client");
+    url = "";
+    option = null;
+  }
+  try {
+    const response = await axios.get(`${url}/api/users/currentuser`, option);
+
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return { currentUser: null };
+  }
+};
+
+export default LandingPage;
