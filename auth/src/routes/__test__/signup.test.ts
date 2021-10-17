4edaf763f7b34439229696be0a79480a62fc9079
Eric Tam <eric.m2hk@gmail.com>
2021-10-17 05:03:30 -0700

    test around signup functionality

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,13 +1,73 @@
 import request from "supertest";
 import { app } from "../../app";
 
+const validUser = {
+  email: "user1@mail.com",
+  password: "P4ssword",
+};
+
+interface inputUser {
+  email?: string;
+  password?: string;
+}
+
+const signupUser = (user: inputUser = { ...validUser }) => {
+  return request(app).post("/api/users/signup").send(user);
+};
+
 describe("signup", () => {
   it("return a 201 on successful signup", async () => {
-    const response = await request(app).post("/api/users/signup").send({
-      email: "test@test.com",
-      password: "password",
-    });
+    const response = await signupUser();
 
     expect(response.status).toBe(201);
+  });
+
+  it("return a 400 with an invalid email", async () => {
+    const response = await signupUser({
+      email: "abctest",
+      password: validUser.password,
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it("return a 400 with an invalid password", async () => {
+    const response = await signupUser({
+      email: validUser.email,
+      password: "bab",
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it("return a 400 with missing password", async () => {
+    const response = await signupUser({ email: validUser.email });
+
+    expect(response.status).toBe(400);
+  });
+
+  it("return a 400 with missing email", async () => {
+    const response = await signupUser({ password: validUser.password });
+
+    expect(response.status).toBe(400);
+  });
+
+  it("return a 400 with missing email and password", async () => {
+    const response = await signupUser({});
+
+    expect(response.status).toBe(400);
+  });
+
+  it("disallows duplicate emails", async () => {
+    await signupUser().expect(201);
+    const response = await signupUser();
+
+    expect(response.status).toBe(400);
+  });
+
+  it("sets a cookie after successful signup", async () => {
+    const response = await signupUser().expect(201);
+
+    expect(response.get("Set-Cookie")).toBeDefined();
   });
 });
