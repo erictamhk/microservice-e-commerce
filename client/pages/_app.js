74159758721465707b7ae6fd375819a528ad8e99
Eric Tam <eric.m2hk@gmail.com>
2021-10-17 17:03:34 -0700

    getting current user from appComponent

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,44 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-const app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Headers! {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default app;
+AppComponent.getInitialProps = async (appContext) => {
+  console.log("app component getInitialProps");
+
+  let pageProps = {};
+  try {
+    if (appContext.Component.getInitialProps) {
+      pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  let currentUser = null;
+  try {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get(`/api/users/currentuser`);
+
+    currentUser = data.currentUser;
+  } catch (err) {
+    console.error(err);
+  }
+
+  const returnData = {
+    pageProps,
+    currentUser,
+  };
+
+  console.log(returnData);
+
+  return returnData;
+};
+
+export default AppComponent;
