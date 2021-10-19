b41d4ca5d3d0994b1299350883699d1b34b2bbb1
Eric Tam <eric.m2hk@gmail.com>
2021-10-19 04:30:58 -0700

    using generic event type

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import { Message, Stan } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { Subjects } from "./subjests";
+import { TicketCreatedEvent } from "./ticket-created-event";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payments-service";
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("event data", data);
 
     msg.ack();
