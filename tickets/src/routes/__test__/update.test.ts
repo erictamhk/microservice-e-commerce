3c9f34f390161b69caf0f780307ca4b21d5dd401
Eric Tam <eric.m2hk@gmail.com>
2021-10-18 18:50:25 -0700

    test for update ticket

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { app } from "../../app";
+import {
+  successTicket,
+  validTicket,
+  InputTicket,
+  InputOptions,
+} from "../../test/setup";
+import { Ticket } from "../../models/ticket";
+
+const updateTicket = (
+  ticketId: string = "test-ticket-id",
+  ticket: InputTicket = {},
+  options: InputOptions = {}
+) => {
+  const agent = request(app).put(`/api/tickets/${ticketId}`);
+  if (options.cookie) {
+    agent.set("Cookie", options.cookie);
+  }
+  return agent.send(ticket);
+};
+
+describe("update", () => {
+  it("returns a 404 if the provided id does not exist", async () => {
+    await updateTicket().expect(404);
+  });
+  // it("returns a 401 if the user is not authenticated", async () => {});
+  // it("returns a 401 if the user dose not own the ticket", async () => {});
+  // it("return a 400 if the user provides an invalid title or price", async () => {});
+  // it("updates the ticket provided valid inputs", async () => {});
+});
