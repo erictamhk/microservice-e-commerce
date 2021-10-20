b9cfbbd7b6cec9629050b665e77eabe31d02a76a
Eric Tam <eric.m2hk@gmail.com>
2021-10-19 21:18:08 -0700

    delete orders

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,9 +1,68 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Order, OrderDoc, OrderStatus } from "../../models/order";
+import { Ticket, TicketDoc } from "../../models/ticket";
+import { getValidCookie } from "../../test/setup";
+
+const createTickets = async (): Promise<TicketDoc> => {
+  const ticket = Ticket.build({
+    title: `concert-test`,
+    price: 20,
+  });
+  await ticket.save();
+  return ticket;
+};
+
+const createOrder = async (
+  ticket: TicketDoc,
+  userId: string
+): Promise<OrderDoc> => {
+  const order = Order.build({
+    ticket,
+    userId,
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+  return order;
+};
 
 describe("delete order", () => {
-  // it("", async () => {});
-  // it("", async () => {});
+  it("marks an order as cancelled", async () => {
+    const user = {
+      email: "user1@mail.com",
+      id: "user1",
+    };
+    const ticket = await createTickets();
+    const order = await createOrder(ticket, user.id);
+
+    const response = await request(app)
+      .delete(`/api/orders/${order.id}`)
+      .set("Cookie", getValidCookie(user))
+      .send({})
+      .expect(200);
+    expect(response.body.id).toEqual(order.id);
+    expect(response.body.ticket.id).toEqual(ticket.id);
+    expect(response.body.status).toEqual(OrderStatus.Cancelled);
+  });
+  it("returns an error if one user tries to delete another users order", async () => {
+    const user1 = {
+      email: "user1@mail.com",
+      id: "user1",
+    };
+    const user2 = {
+      email: "user2@mail.com",
+      id: "user2",
+    };
+    const ticket = await createTickets();
+    const order = await createOrder(ticket, user1.id);
+
+    const response = await request(app)
+      .delete(`/api/orders/${order.id}`)
+      .set("Cookie", getValidCookie(user2))
+      .send({})
+      .expect(401);
+  });
   // it("", async () => {});
   // it("", async () => {});
 });
