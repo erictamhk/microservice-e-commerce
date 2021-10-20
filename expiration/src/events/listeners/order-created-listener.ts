cd461eb318d0f8eae80e6a9784dab9e358ee9e93
Eric Tam <eric.m2hk@gmail.com>
2021-10-20 04:23:38 -0700

    delay the queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,7 +8,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add({ orderId: data.id });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("waiting this many milliseconds to process the job", delay);
+
+    await expirationQueue.add(
+      { orderId: data.id },
+      {
+        delay: delay,
+      }
+    );
 
     msg.ack();
   }
