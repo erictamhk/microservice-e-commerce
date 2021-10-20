3d133e274a249bdc6a2abb0506c87d1a1e930d38
Eric Tam <eric.m2hk@gmail.com>
2021-10-20 04:05:08 -0700

    add a queue when have a order created event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,15 @@
 import { Message } from "node-nats-streaming";
 import { Subjects, Listener, OrderCreatedEvent } from "@sgtickets/common";
 import { queueGroupName } from "./quene-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({ orderId: data.id });
+
+    msg.ack();
+  }
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
   if (!process.env.NATS_URL) {
@@ -22,6 +23,8 @@ const start = async () => {
       console.log("NATs connection closed!");
       process.exit();
     });
+
+    new OrderCreatedListener(natsWrapper.client).listen();
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
