c49f4505764a6b464602c8feb69cf1fae7f30ff6
Eric Tam <eric.m2hk@gmail.com>
2021-10-20 05:22:16 -0700

    add payments service to project

---
+++ payments/src/index.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be definded");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be definded");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be definded");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be definded");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be definded");
+  }
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    console.log("connected to NATs");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATs connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (err) {
+    console.error(err);
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log("payments v1");
+    console.log("Listening to port 3000");
+  });
+};
+
+start();
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,76 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import jwt from "jsonwebtoken";
+import { app } from "../app";
+
+jest.mock("../nats-wrapper");
+
+let mongo: MongoMemoryServer;
+beforeAll(async () => {
+  process.env.JWT_KEY = "testing-key";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+export const validTicket = {
+  title: "test title",
+  price: 100.0,
+};
+
+export interface InputTicket {
+  title?: string;
+  price?: number;
+}
+
+export interface InputOptions {
+  cookie?: string[];
+}
+
+export const postTicket = (
+  ticket: InputTicket = { ...validTicket },
+  options: InputOptions = {}
+) => {
+  const agent = request(app).post("/api/tickets");
+  if (options.cookie) {
+    agent.set("Cookie", options.cookie);
+  }
+  return agent.send(ticket);
+};
+
+export const successTicket = () => {
+  return postTicket({ ...validTicket }, { cookie: getValidCookie() });
+};
+
+export const getValidCookie = (
+  payload = {
+    email: "user1@mail.com",
+    id: "payload-id",
+  }
+): string[] => {
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`express:sess=${base64}`];
+};
