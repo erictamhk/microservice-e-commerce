3826400cc18faa540b0627e461ccfc22cf81324c
Eric Tam <eric.m2hk@gmail.com>
2021-10-20 19:27:06 -0700

    display the time left of order

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState("");
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
     <div>
       <h1>Order</h1>
       <h4>Id: {order.id}</h4>
+      <h4>Time left to pay: {timeLeft} seconds</h4>
     </div>
   );
 };
