30fd02b451572d5ed282ddb1d9601ca0c051ef9c
Eric Tam <eric.m2hk@gmail.com>
2021-10-20 18:38:57 -0700

    refactoring index

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,24 +6,25 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
   );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
   let pageProps = {};
-  try {
-    if (appContext.Component.getInitialProps) {
-      pageProps = await appContext.Component.getInitialProps(appContext.ctx);
-    }
-  } catch (err) {
-    console.error(err);
-  }
   let currentUser = null;
   try {
     const client = buildClient(appContext.ctx);
     const { data } = await client.get(`/api/users/currentuser`);
+
+    if (appContext.Component.getInitialProps) {
+      pageProps = await appContext.Component.getInitialProps(
+        appContext.ctx,
+        client,
+        data.currentUser
+      );
+    }
 
     currentUser = data.currentUser;
   } catch (err) {
