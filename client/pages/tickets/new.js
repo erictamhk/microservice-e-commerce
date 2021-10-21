3913dc7d37c6f74a5034add3000f1545c18d11f3
Eric Tam <eric.m2hk@gmail.com>
2021-10-20 18:49:18 -0700

    add a new ticket page

---
+++ client/pages/tickets/new.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const NetTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  const onBlur = () => {
+    const value = Number(price);
+    if (isNaN(value)) {
+      return;
+    }
+    setPrice(value.toFixed(2));
+  };
+
+  return (
+    <div>
+      <h1>Create a Ticket</h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input
+            value={price}
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default NetTicket;
